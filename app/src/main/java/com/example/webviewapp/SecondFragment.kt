469bfa8c1211f6d0cdaa7e96c72b36a988073bda
fiotto <fiotto42@gmail.com>
2020-12-12 23:15:44 +0900

    add WebView

--- app/src/main/java/com/example/webviewapp/SecondFragment.kt
+++ app/src/main/java/com/example/webviewapp/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
@@ -18,7 +19,13 @@ class SecondFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_second, container, false)
+
+        //xmlのWebViewで指定したid
+        val myWebView: WebView = view.findViewById(R.id.webview)
+        myWebView.loadUrl("https://m.yahoo.co.jp")
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
