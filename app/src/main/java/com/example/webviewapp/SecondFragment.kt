d19d8bf8d855297cb699865acf8da25d48d87586
fiotto <fiotto42@gmail.com>
2020-12-16 01:54:41 +0900

    fix webview methods post

--- app/src/main/java/com/example/webviewapp/SecondFragment.kt
+++ app/src/main/java/com/example/webviewapp/SecondFragment.kt
@@ -1,12 +1,13 @@
 package com.example.webviewapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
@@ -21,9 +22,26 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View =  inflater.inflate(R.layout.fragment_second, container, false)
 
-        //xmlのWebViewで指定したid
         val myWebView: WebView = view.findViewById(R.id.webview)
-        myWebView.loadUrl("http://10.0.2.2:8080/")
+        myWebView.settings.javaScriptEnabled = true // JavaScriptを有効にする
+        WebView.setWebContentsDebuggingEnabled(true) // インスタンスではなく、プロパティメソッド
+
+        val postData = ""
+        myWebView.postUrl("http://10.0.2.2:8080/", postData.toByteArray());
+
+        val id = "1234567890"
+        val name = "fiotto"
+
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(webView: WebView, url: String) {
+                super.onPageFinished(webView, url)
+
+                val script = "window.setValue('$id','$name')"
+                println(script)
+
+                webView.evaluateJavascript(script, null)
+            }
+        }
 
         return view
     }
